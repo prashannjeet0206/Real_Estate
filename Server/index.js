2865099bd88e1ed91458ae4076965d8713875bc8
prashannjeet0206 <prashannjeet0206@gmail.com>
2024-07-29 00:55:01 +0530

    create user model

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+// Databse
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connection success");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// Server
+app.listen(PORT, () => {
   console.log("Server");
 });
