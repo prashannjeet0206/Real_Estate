b52261e5f9f5c98c669ba659843eba7e15f225df
prashannjeet0206 <prashannjeet0206@gmail.com>
2024-07-31 00:41:03 +0530

    Created middleware to handle error

--- Server/controller/auth.controller.js
+++ Server/controller/auth.controller.js
@@ -1,9 +1,19 @@
 import newUser from "../Models/user.model.js";
+import bcrypt from "bcryptjs";
 
-export const Signup = async (req, res) => {
-  const { name, email, password } = req.body;
-  const createUser = await newUser.create({ name, email, password });
-  res
-    .status(201)
-    .json({ success: true, message: "New user Created", data: createUser });
+export const Signup = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const createUser = await newUser.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    res
+      .status(201)
+      .json({ success: true, message: "New user Created", data: createUser });
+  } catch (error) {
+    next(error);
+  }
 };
--- Server/index.js
+++ Server/index.js
@@ -26,3 +26,14 @@ mongoose
 app.listen(PORT, () => {
   console.log(`Server listening to Port:${PORT}`);
 });
+
+// middleware for errors
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
